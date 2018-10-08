3a31f8b70c9941a9b3af0c9bf1efbdc923da9101
Marcin <elomelo302@gmail.com>
2018-10-08 18:17:43 +0200

    menu debuging

--- src/views/homePageView/Components/Menu/Menu.jsx
+++ src/views/homePageView/Components/Menu/Menu.jsx
@@ -41,8 +41,9 @@ export class Menu extends Component {
           let scroll = {    
              y: supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop
           };
-          
+
         if ( window.location.pathname === '/pokedex' ) {
+            console.log(window.location.pathname)
             this.setState({ changeMenuStyle: scroll.y > 350 ? true : false })
             setTimeout( () => this.setState({ searchHidden: scroll.y > 350 ? false : true }),500 )
         }
